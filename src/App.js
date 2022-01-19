f750a7b51d7aef775c1b11553d345575de27d65f
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-01-19 08:43:40 -0300

    Updated Styles
    - Improved styles on mobile.

--- src/App.js
+++ src/App.js
@@ -108,21 +108,49 @@ const App = () => {
   return (
     <>
       <Flex w='100%' h="100vh">
-        <Flex w='100%' flexDir='column' ml='20%' mt='5%' mr='20%' color='#fff'>
-          <Text fontWeight='700' textAlign='center' mb='10%' fontSize={30}>What's the Plan for Today?</Text>
+        <Flex justify='center' align='center' flexDir='column' w='100%' maxW='70%' m='40px' color='#fff'>
+          <Text fontWeight='700' textAlign='center' mb='10%' fontSize={30}
+            sx={{
+              '@media screen and (max-width: 500px)': {
+                fontSize: '20px',
+              },
+            }}>
+            What's the Plan for Today?
+          </Text>
           <form onSubmit={addTask}>
-            <Flex mt='2%'>
-              <Input value={newTask} onChange={e => setNewTask(e.target.value)} focusBorderColor='purple.400' variant='outline' w='50%' placeholder='Task to be done' />
+            <Flex mt='2%'
+              sx={{
+                '@media screen and (max-width: 500px)': {
+                  marginBottom: '30px',
+                },
+              }}
+            >
+              <Input
+                value={newTask} onChange={e => setNewTask(e.target.value)} focusBorderColor='purple.400' variant='outline' w='50%' placeholder='Task to be done'
+                sx={{
+                  '@media screen and (max-width: 500px)': {
+                    w: '60%',
+                  },
+                }}
+              />
               <Button onClick={addTask} ml={5} bg='purple.400' _hover={{ bg: 'purple.300' }} _focus={{ boxShadow: 'none' }}><SmallAddIcon w={5} h={5} /></Button>
             </Flex>
           </form>
           <Tabs mt='2%' w='100%'>
             <TabList >
-              <Tab _focus='none' color='purple.200' _selected={{ color: 'purple.400', borderColor: 'purple.400' }}>Incomplete Tasks</Tab>
-              <Tab _focus='none' color='purple.200' _selected={{ color: 'purple.400', borderColor: 'purple.400' }}>Completed Tasks</Tab>
+              <Tab _focus='none' color='purple.200' _selected={{ color: 'purple.400', borderColor: 'purple.400' }}>Incomplete</Tab>
+              <Tab _focus='none' color='purple.200' _selected={{ color: 'purple.400', borderColor: 'purple.400' }}>Completed</Tab>
             </TabList>
             <TabPanels>
-              <TabPanel>
+              <TabPanel maxH='400px' overflowY='scroll'
+                css={{
+                  '&::-webkit-scrollbar': {
+                    width: '4px',
+                  },
+                  '&::-webkit-scrollbar-thumb': {
+                    background: 'rgba(255, 255, 255, 0.16)',
+                  },
+                }}>
                 {tasks.map((task, index) => (
                   !task.isChecked ? <TaskItem removeTask={removeTask} updateTask={updateTask} key={index} task={task} index={index} /> : null
                 ))}
